1692c041c25ae4b5dae8bef9b417ef8f6baa7e7a
KristaJaanus <krista.jaanus@tptlive.ee>
2016-11-18 09:46:59 +0200

    Muudatused2

---
+++ src/Chapter6/oddSum.java
@@ -0,0 +1,36 @@
+package Chapter6;
+/**
+ * Created by Krista
+ */
+public class oddSum {
+    public static void main(String[]args){
+        int n = 2;
+
+        if(n <= 0){
+            System.out.println("Negatiivne number");
+        }
+        else
+        {
+           oddSum(n);
+        }
+        System.out.println(oddSum(n));
+    }
+    public static int oddSum(int n) {
+        if (n <= 0)
+        {
+            return 0;
+        }
+        if (n % 2 == 0) {
+            return ((n - 1) + oddSum(n - 2));
+        }
+                else
+        {
+
+                return (n + oddSum(n - 2));
+            }
+        }
+
+    }
+
+
+
